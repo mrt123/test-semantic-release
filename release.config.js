91e8d389edc30247bbb797b02e97936082ea6526
Tomasz Szawara <tomasz.szawara@setapp.pl>
2020-02-05 17:33:45 +0100

    feat(pencil): add feature1.js

--- release.config.js
+++ release.config.js
@@ -1,10 +1,11 @@
 module.exports = {
+  repositoryUrl: "https://github.com/mrt123/test-semantic-release",
   branches: [
     {name: 'master'},
     {name: 'develop', prerelease: 'develop'},
   ],
   ci: false,
-  dryRun: true,
+  dryRun: false,  // Dry-run mode, skip publishing, print next version and release notes
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/github",
